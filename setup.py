14339e58aeb350d9413a154317ea1205cdcaad02
Charles H Camp Jr <charles.camp@nist.gov>
2017-12-07 03:55:24 -0500

    Changed version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as f:
       long_description = f.read()
 
 setup(name='sciplot-pyqt',
-      version = '0.1.5a3',
+      version = '0.1.5b1',
       description = 'A small matplotlib wrapper/UI for creating \
                      publication-ready plots, graphs, and images',
       long_description = long_description,
