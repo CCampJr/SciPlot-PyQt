9e9254f9fd7ce65f10f99822ff4a1eea33c2da0c
Charles H Camp Jr <charles.camp@nist.gov>
2017-12-06 23:24:29 -0500

    [CI] Trying apt-get pyqt5

--- .travis/build_pyqt5.sh
+++ .travis/build_pyqt5.sh
@@ -10,24 +10,27 @@ sudo apt-get update
 sudo apt-get install -y qt59base
 source /opt/qt59/bin/qt59-env.sh
 
-mkdir -p $PYQT
-cd $PYQT
+sudo apt-get install -y python3-pyqt5
+sudo apt-get install -y qttools5-dev-tools
+sudo apt-get install -y pyqt5-dev-tools
+# mkdir -p $PYQT
+# cd $PYQT
 
-wget -O sip.tar.gz http://sourceforge.net/projects/pyqt/files/sip/sip-$SIP_VERSION/sip-$SIP_VERSION.tar.gz
-mkdir -p sip
-tar xzf sip.tar.gz -C sip --strip-component=1
+# wget -O sip.tar.gz http://sourceforge.net/projects/pyqt/files/sip/sip-$SIP_VERSION/sip-$SIP_VERSION.tar.gz
+# mkdir -p sip
+# tar xzf sip.tar.gz -C sip --strip-component=1
 
-wget -O PyQt.tar.gz  http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-$PYQT_VERSION/PyQt5_gpl-$PYQT_VERSION.tar.gz
-mkdir -p PyQt
-tar xzf PyQt.tar.gz -C PyQt --strip-components=1
+# wget -O PyQt.tar.gz  http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-$PYQT_VERSION/PyQt5_gpl-$PYQT_VERSION.tar.gz
+# mkdir -p PyQt
+# tar xzf PyQt.tar.gz -C PyQt --strip-components=1
 
-cd $PYQT/sip
-#python configure.py -e $PYQT/include
-python configure.py
-make
-sudo make install
+# cd $PYQT/sip
+# #python configure.py -e $PYQT/include
+# python configure.py
+# make
+# sudo make install
 
-cd $PYQT/PyQt
-python configure.py --confirm-license --no-designer-plugin -e QtCore -e QtGui -e QtWidgets
-make
-sudo make install
+# cd $PYQT/PyQt
+# python configure.py --confirm-license --no-designer-plugin -e QtCore -e QtGui -e QtWidgets
+# make
+# sudo make install
