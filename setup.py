46bb78ba0a906e8675eeb3c08845efea76eeac4e
Charles H Camp Jr <charles.camp@nist.gov>
2019-05-17 18:16:05 -0400

    Version to 0.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='sciplot-pyqt',
-      version = '0.2.2rc1',
+      version = '0.2.2',
       description = 'A small matplotlib wrapper/UI for creating \
                      publication-ready plots, graphs, and images',
       long_description = long_description,
